a7cb71deda09a5a632cdc9971fbb8e5f35796eb1
Sean Nealon <signalflowsean@gmail.com>
2019-03-06 17:21:11 -0500

    loadwebcam is inside of a function

--- skeleton-prediction.js
+++ skeleton-prediction.js
@@ -9,10 +9,14 @@ const flipHorizontal = false;
 
 const webcam = document.getElementById('webcam');
 
-if(navigator.mediaDevices.getUserMedia) { 
-  navigator.mediaDevices.getUserMedia({video : true})
-    .then(stream => webcam.srcObject = stream); 
+const loadWebcam = () => { 
+  if(navigator.mediaDevices.getUserMedia) { 
+    navigator.mediaDevices.getUserMedia({video : true})
+      .then(stream => webcam.srcObject = stream); 
+  }
 }
+
+loadWebcam(); 
 
 // posenet.load().then(net => { 
 //   return net.estimateSinglePose(catImage, imageScaleFactor, flipHorizontal, outputStride)
