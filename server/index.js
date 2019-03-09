ce6aa27d5cf394c1d3be438c5716d82b6e431629
Sean Nealon <signalflowsean@gmail.com>
2019-03-09 13:02:02 -0500

    added a position boilerplate route

--- server/index.js
+++ server/index.js
@@ -6,20 +6,12 @@ const app = express();
 
 const { PORT, CLIENT_ORIGIN } = require('./config'); 
 
+const positionRouter = require('./routes/position'); 
+
 app.use(bodyParser.json());
 app.use(cors({origin: CLIENT_ORIGIN})); 
 app.use(morgan('dev')); 
 
-app.get('/api/test', (req, res) => res.send('Test Route')); 
-
-app.post('/api/position/:person', (req, res) => { 
-  //TODO: Post the person's position to storage
-  res.send('....'); 
-}); 
-
-app.get('/api/position', (req, res) => {{ 
-  //TODO: Get all of the people's position who are active in the session
-  res.send('....'); 
-}}); 
+app.use('/api/position', positionRouter); 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
