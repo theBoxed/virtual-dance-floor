d33e01a0683488c45f8ba1ef33e8c14096f97f75
Sean Nealon <signalflowsean@gmail.com>
2019-03-14 10:48:42 -0400

    users are being stored

--- firebase-test/scripts/app.js
+++ firebase-test/scripts/app.js
@@ -8,16 +8,23 @@ var config = {
 };
 firebase.initializeApp(config);
 
-let  VIRTUAL_DANCE_FLOOR = firebase.database().ref(); 
+let VIRTUAL_DANCE_FLOOR = firebase.database().ref(); 
+let person = VIRTUAL_DANCE_FLOOR.child(userId).push().key;
 console.log('database', VIRTUAL_DANCE_FLOOR); 
 
 
 function writeUserData(x, y, userId) {
   console.log('userdata', x, y, userId); 
-  firebase.database().ref().set({
+  firebase.database().ref(userId).set({
     userId, userId, 
     x: x,
     y: y,
   });
 }
+
+// var xPosition = firebase.database().ref(x);
+// xPosition.on('value', function(snapshot) {
+//   updateStarCount(postElement, snapshot.val());
+//   console.log('snapshot', snapshot); 
+// });
 console.log('database', VIRTUAL_DANCE_FLOOR); 
