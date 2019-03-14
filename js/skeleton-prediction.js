21e8418f22232f2dab834b115ca8c4474966cd1d
Box <40639989+theBoxed@users.noreply.github.com>
2019-03-14 19:10:27 -0400

    combined collaborate-position with skeleton-prediction

--- js/skeleton-prediction.js
+++ js/skeleton-prediction.js
@@ -1,20 +1,66 @@
-  let local;
-  let world = [];
+const config = {
+  apiKey: 'AIzaSyAqtz0eHO33noqIR6CMDPLvEs2dkqBf2Ag',
+  authDomain: 'virtual-dance-floor.firebaseapp.com',
+  databaseURL: 'https://virtual-dance-floor.firebaseio.com',
+  projectId: 'virtual-dance-floor',
+  storageBucket: 'virtual-dance-floor.appspot.com',
+  messagingSenderId: '542923101372'
+};
 
-  //Sets up the canvas and initializes the video.
-  function setup() {
-    createCanvas(800, 600);
-    background(0);
+let dancer;
+const userId = Math.floor(Math.random() * 40000);
 
-    Draw.begin();
-    local = new Dancer();
-  }
+//Sets up the canvas and initializes the video.
+function setup() {
+  firebase.initializeApp(config);
 
-  function draw() {
-    background(0);
-    local.update(Draw.getPose());
-    if (local.data.pose != null) {
-      writeUserData(local.data.pose); 
-      Draw.drawPose(local.data.pose, { color: local.data.color });
+  //Create the intialize obj in fb
+  firebase.database().ref();
+  //Create obj within world to hold all users
+  firebase
+    .database()
+    .ref()
+    .child('users')
+    .push().key;
+  //Setup the canvas
+  createCanvas(800, 600);
+  background(0);
+  //call method to setup video and ml5
+  Draw.begin();
+  dancer = new Dancer();
+}
+
+function writeUserData(joints) {
+  firebase.database().ref(`users/userId:${userId}`).set({ joints });
+}
+
+function loopThroughUsers(users) {
+  console.log('users', users);
+
+  for (prop in users) {
+    console.log('users', users[prop].joints);
+
+    if (prop !== `userId:${userId}`) {
+      Draw.drawPose(users[prop].joints, { color: local.data.color });
     }
   }
+}
+
+function draw() {
+  background(0);
+  //update the skeleton 
+  dancer.update(Draw.getPose());
+  //draw the skeleton
+  if (dancer.data.pose != null) {
+    // writeUserData(dancer.data.pose);
+    Draw.drawPose(dancer.data.pose, { color: dancer.data.color });
+  }
+  //provides firebase with dancer updates
+  writeUserData(dancer.data.pose);
+
+  firebase.database().ref('users/').once('value').then(function (snapshot){
+    loopThroughUsers(snapshot.val());
+  });
+
+
+}
