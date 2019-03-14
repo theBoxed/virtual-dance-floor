6362f0dc6b1ab7c21942165f7f42a9fb94143d02
Sean Nealon <signalflowsean@gmail.com>
2019-03-14 09:33:01 -0400

    added p5 sketch

--- firebase-test/scripts/app.js
+++ firebase-test/scripts/app.js
@@ -1,12 +1,19 @@
+var config = {
+  apiKey: "AIzaSyAqtz0eHO33noqIR6CMDPLvEs2dkqBf2Ag",
+  authDomain: "virtual-dance-floor.firebaseapp.com",
+  databaseURL: "https://virtual-dance-floor.firebaseio.com",
+  projectId: "virtual-dance-floor",
+  storageBucket: "virtual-dance-floor.appspot.com",
+  messagingSenderId: "542923101372"
+};
+firebase.initializeApp(config);
 
-  
-  var config = {
-    apiKey: "AIzaSyAqtz0eHO33noqIR6CMDPLvEs2dkqBf2Ag",
-    authDomain: "virtual-dance-floor.firebaseapp.com",
-    databaseURL: "https://virtual-dance-floor.firebaseio.com",
-    projectId: "virtual-dance-floor",
-    storageBucket: "virtual-dance-floor.appspot.com",
-    messagingSenderId: "542923101372"
-  };
-  firebase.initializeApp(config);
-console.log('hi')
+let  VIRTUAL_DANCE_FLOOR = firebase.database().ref(); 
+console.log('database', VIRTUAL_DANCE_FLOOR); 
+
+VIRTUAL_DANCE_FLOOR.on("value", snapshot => { 
+  console.log('snapshot', snapshot.val()); 
+}); 
+
+
+console.log('database', VIRTUAL_DANCE_FLOOR); 
