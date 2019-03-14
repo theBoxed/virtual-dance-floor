a2050c29ff39ed6e8a258eef78ea747c1727e6c3
Sean Nealon <signalflowsean@gmail.com>
2019-03-14 12:30:39 -0400

    formated writeUserData function to be clearer

--- firebase-test/scripts/app.js
+++ firebase-test/scripts/app.js
@@ -13,11 +13,7 @@ let VIRTUAL_DANCE_FLOOR = firebase.database().ref();
 let users = VIRTUAL_DANCE_FLOOR.child('users').push().key; 
 
 function writeUserData(x, y, angle1, userId) {
-  firebase.database().ref(`users/userId:${userId}`).set({
-    x,
-    y,
-    angle1
-  });
+  firebase.database().ref(`users/userId:${userId}`).set({x, y, angle});
 }
 
 var usersRef = firebase.database().ref('users/');
