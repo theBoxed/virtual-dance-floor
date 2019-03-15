78e39f20a960e23d7228ed803db8f3a8df7d4298
Sean Nealon <signalflowsean@gmail.com>
2019-03-15 19:21:04 -0400

    unindented skeleton-predicition

--- js/skeleton-prediction.js
+++ js/skeleton-prediction.js
@@ -1,38 +1,38 @@
-  let dancer;
-  let dancers = []; 
+let dancer;
+let dancers = []; 
 
-  //Sets up the canvas and initializes the video.
-  function setup() {
-    createCanvas(800, 600);
-    background(0);
+//Sets up the canvas and initializes the video.
+function setup() {
+  createCanvas(800, 600);
+  background(0);
 
-    //sets up dancer
-    let userId = Math.floor(Math.random() * 40000); 
-    dancer = Dancer(userId);
-    console.log('dancer userId', dancer.userId); 
+  //sets up dancer
+  let userId = Math.floor(Math.random() * 40000); 
+  dancer = Dancer(userId);
+  console.log('dancer userId', dancer.userId); 
 
-    //sets up firebase
-    firebase.initializeApp(config);
+  //sets up firebase
+  firebase.initializeApp(config);
+
+  //sets up camera - initializes the video
+  Scene().start();
+  dancer = Dancer(userId); 
+}
+
+function draw() {
+  background(0);
   
-    //sets up camera - initializes the video
-    Scene().start();
-    dancer = Dancer(userId); 
-  }
+  dancer.update();
+}
 
-  function draw() {
-    background(0);
-    
-    dancer.update();
+window.onbeforeunload = function (event) {
+  var message = 'Important: Please click on \'Save\' button to leave this page.';
+  if (typeof event == 'undefined') {
+      event = window.event;
   }
-
-  window.onbeforeunload = function (event) {
-    var message = 'Important: Please click on \'Save\' button to leave this page.';
-    if (typeof event == 'undefined') {
-        event = window.event;
-    }
-    if (event) {
-        //Browser window is closing
-        event.returnValue = message;
-    }
-    return message;
+  if (event) {
+      //Browser window is closing
+      event.returnValue = message;
+  }
+  return message;
 };
