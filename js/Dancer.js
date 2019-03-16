799e76cc60421de255d18d414a9ba8de1ef7b7f8
Sean Nealon <signalflowsean@gmail.com>
2019-03-16 11:17:48 -0400

    replaced vars with lets

--- js/Dancer.js
+++ js/Dancer.js
@@ -22,9 +22,9 @@ const Dancer = (id, x, y) => {
   }
 
   const _convertPose = (posenet_obj) => { 
-    var result = {}
-    var kpts = posenet_obj.pose.keypoints
-    for (var i = 0; i < kpts.length; i++) {
+    let result = {}
+    let kpts = posenet_obj.pose.keypoints
+    for (let i = 0; i < kpts.length; i++) {
       result[kpts[i].part] = {
         x: width - kpts[i].position.x,
         y: kpts[i].position.y
@@ -34,7 +34,7 @@ const Dancer = (id, x, y) => {
   }
 
   const _lerpPose = (poseA, poseB, t) => {
-    for (var k in poseB) {
+    for (let k in poseB) {
       if (isNaN(poseA[k].x)) {
         poseA[k].x = poseB[k].x
       } else {
@@ -51,7 +51,7 @@ const Dancer = (id, x, y) => {
   const _updatePose = (results) => {
     posenetObjs = results;
     if (results.length > 0) {
-      var newPose = _convertPose(_getLargestPosenetObj(results));
+      let newPose = _convertPose(_getLargestPosenetObj(results));
       if (pose0 == null) {
         pose0 = newPose
       } else {
@@ -61,13 +61,13 @@ const Dancer = (id, x, y) => {
   }
 
   const _getLargestPosenetObj = (objs) => {
-    var max_i = 0;
-    var max_d = 0;
-    for (var i = 0; i < objs.length; i++) {
-      var kpts = objs[i].pose.keypoints;
-      var nose = kpts[0]
-      var leftEye = kpts[1]
-      var d = dist(nose.x, nose.y, leftEye.x, leftEye.y);
+    let max_i = 0;
+    let max_d = 0;
+    for (let i = 0; i < objs.length; i++) {
+      let kpts = objs[i].pose.keypoints;
+      let nose = kpts[0]
+      let leftEye = kpts[1]
+      let d = dist(nose.x, nose.y, leftEye.x, leftEye.y);
       if (d > max_d) {
         max_d = d;
         max_i = i;
@@ -86,17 +86,17 @@ const Dancer = (id, x, y) => {
   }
 
   const _drawHead = (pose) => {
-    var ang = atan2(pose.leftEar.y - pose.rightEar.y, pose.leftEar.x - pose.rightEar.x);
-    var r = dist(pose.leftEar.x, pose.leftEar.y, pose.rightEar.x, pose.rightEar.y);
+    let ang = atan2(pose.leftEar.y - pose.rightEar.y, pose.leftEar.x - pose.rightEar.x);
+    let r = dist(pose.leftEar.x, pose.leftEar.y, pose.rightEar.x, pose.rightEar.y);
     arc((pose.leftEar.x + pose.rightEar.x) / 2, (pose.leftEar.y + pose.rightEar.y) / 2, r, r, ang, ang + PI);
-    var neck = { x: (pose.leftShoulder.x + pose.rightShoulder.x) / 2, y: (pose.leftShoulder.y + pose.rightShoulder.y) / 2, }
+    let neck = { x: (pose.leftShoulder.x + pose.rightShoulder.x) / 2, y: (pose.leftShoulder.y + pose.rightShoulder.y) / 2, }
     line(pose.leftEar.x, pose.leftEar.y, neck.x, neck.y);
     line(pose.rightEar.x, pose.rightEar.y, neck.x, neck.y);
     fill(255);
   }
 
   const _drawFace = (pose) => {
-    var s = _estimateSize(pose);
+    let s = _estimateSize(pose);
     fill(255);
     ellipse(pose.leftEye.x, pose.leftEye.y, s * 0.8, s * 0.8);
     ellipse(pose.rightEye.x, pose.rightEye.y, s * 0.8, s * 0.8);
