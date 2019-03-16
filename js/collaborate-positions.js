b5a221433b15f86ac54ee9d09530f2ba8276b4a1
Sean Nealon <signalflowsean@gmail.com>
2019-03-16 11:42:41 -0400

    moved config into skeleton-prediction

--- js/collaborate-positions.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyAqtz0eHO33noqIR6CMDPLvEs2dkqBf2Ag",
-  authDomain: "virtual-dance-floor.firebaseapp.com",
-  databaseURL: "https://virtual-dance-floor.firebaseio.com",
-  projectId: "virtual-dance-floor",
-  storageBucket: "virtual-dance-floor.appspot.com",
-  messagingSenderId: "542923101372"
-};
--- js/skeleton-prediction.js
+++ js/skeleton-prediction.js
@@ -24,6 +24,15 @@ function draw() {
   dancer.update();
 }
 
+var config = {
+  apiKey: "AIzaSyAqtz0eHO33noqIR6CMDPLvEs2dkqBf2Ag",
+  authDomain: "virtual-dance-floor.firebaseapp.com",
+  databaseURL: "https://virtual-dance-floor.firebaseio.com",
+  projectId: "virtual-dance-floor",
+  storageBucket: "virtual-dance-floor.appspot.com",
+  messagingSenderId: "542923101372"
+};
+
 // window.onbeforeunload = function (event) {
 //   var message = 'Important: Please click on \'Save\' button to leave this page.';
 //   if (typeof event == 'undefined') {
