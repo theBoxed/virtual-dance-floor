c98bd82b3173f91bd3150e13460cc5a3a86b18a8
Sean Nealon <signalflowsean@gmail.com>
2019-03-16 11:02:52 -0400

    hid video at the end of setup

--- js/scene.js
+++ js/scene.js
@@ -7,8 +7,12 @@ Scene = () => {
 
     //initializes posenet
     poseNet = ml5.poseNet(video); 
-    
+
+    //checks to see if everything has loaded
     if (!poseNet || !video ) throw new Error('poseNet and or video did not load'); 
+
+    //hides video
+    video.hide(); 
   }
 
   const getPoseNet = () => { 
