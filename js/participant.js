4d7c5eea9679470540e168d81262534d77dfa368
Sean Nealon <signalflowsean@gmail.com>
2019-03-17 15:29:37 -0400

    got rid of unnseary code in participants

--- js/participant.js
+++ js/participant.js
@@ -21,65 +21,14 @@ const Participant = () => {
       .ref(`users/${userId}`)
       .once('value')
       .then(snapshot => {
-        // console.log('snapshot', snapshot.val());
+        console.log('snapshot', userId, snapshot.val().pose);
+        pose = snapshot.val().pose; 
+        _drawPose(pose, {color})
       });
-    // _drawPose(pose, {color})
+     
   }
 
-  const _convertPose = (posenet_obj) => {
-    let result = {}
-    let kpts = posenet_obj.pose.keypoints
-    for (let i = 0; i < kpts.length; i++) {
-      result[kpts[i].part] = {
-        x: width - kpts[i].position.x,
-        y: kpts[i].position.y
-      }
-    }
-    return result;
-  }
 
-  const _lerpPose = (poseA, poseB, t) => {
-    for (let k in poseB) {
-      if (isNaN(poseA[k].x)) {
-        poseA[k].x = poseB[k].x
-      } else {
-        poseA[k].x = lerp(poseA[k].x, poseB[k].x, t);
-      }
-      if (isNaN(poseA[k].y)) {
-        poseA[k].y = poseB[k].y
-      } else {
-        poseA[k].y = lerp(poseA[k].y, poseB[k].y, t);
-      }
-    }
-  }
-
-  const _updatePose = (results) => {
-    posenetObjs = results;
-    if (results.length > 0) {
-      let newPose = _convertPose(_getLargestPosenetObj(results));
-      if (pose == null) {
-        pose = newPose
-      } else {
-        _lerpPose(pose, newPose, 0.3);
-      }
-    }
-  }
-
-  const _getLargestPosenetObj = (objs) => {
-    let max_i = 0;
-    let max_d = 0;
-    for (let i = 0; i < objs.length; i++) {
-      let kpts = objs[i].pose.keypoints;
-      let nose = kpts[0]
-      let leftEye = kpts[1]
-      let d = dist(nose.x, nose.y, leftEye.x, leftEye.y);
-      if (d > max_d) {
-        max_d = d;
-        max_i = i;
-      }
-    }
-    return objs[max_i];
-  }
 
   const _estimateSize = (pose) => {
     return dist(pose.nose.x, pose.nose.y, pose.leftEye.x, pose.leftEye.y);
@@ -113,6 +62,7 @@ const Participant = () => {
   }
 
   const _drawPose = (pose, args) => {
+    console.log('hi')
     if (args == undefined) { args = {} }
     if (args.color == undefined) { args.color = [255, 255, 255] }
     push();
