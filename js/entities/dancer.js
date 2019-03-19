2c28043a04b5ad4761beff88b8e2c2276b3d19e3
Sean Nealon <signalflowsean@gmail.com>
2019-03-19 10:26:37 -0400

    cleaned up dancer initalization

--- js/entities/dancer.js
+++ js/entities/dancer.js
@@ -1,25 +1,23 @@
 const Dancer = () => { 
-  let dancer = Object.assign({}, _draw(), _poseNet()); 
-  dancer.id = null; 
-  dancer.pose = null; 
-  dancer.color = [100]; 
-  dancer.done = false; 
-  dancer.isReady = false; 
-  dancer.posenetObjs = {}; 
+
+  let dancer = Object.assign(
+    { id: null, pose: null, color: [100], done: false, 
+      isReady: false, posenetObjs: {} 
+    }, _draw(), _poseNet()); 
 
   dancer.update = () => { 
     //write pose to firebase
     firebase.database().ref(`users/${this.id}`)
       .set({pose : this.pose});
-      
+
+    //draw pose
     dancer.drawPose(this.pose)
   }
 
   dancer.initialize = poseNet => { 
     //set up random userid
     this.id = Math.floor(Math.random() * 40000); 
-
-
+    
     //contiously find new pose
     poseNet.on('pose', results => { 
       dancer.pose = results; 
