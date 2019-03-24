e84e2e2cc503bef2ab404a0382949b5df4e2febc
Sean Nealon <signalflowsean@gmail.com>
2019-03-24 14:36:58 -0400

    if the number of users has changed the _numUsersHasChanged method gets called

--- js/utils/_userManagement.js
+++ js/utils/_userManagement.js
@@ -11,14 +11,19 @@ const manageUsers = () => {
       .ref('users')
       .once('value')
       .then(snapshot => {
-        console.log('users', snapshot.val()); 
-        //go through participants 
-        //go through keys in snap shot
-        //if a key is in snapshot but not participants
-        _addUser(); 
-        //if a key is in participants but not snapshot
-        _removeUser(); 
+        if (user.length !== participants){ 
+          _numUsersHasChanged(snapshot.val()); 
+        }
       });  
+  }
+
+  const _numUsersHasChanged = (users) => { 
+    //go through participants 
+    //go through keys in snap shot
+    //if a key is in snapshot but not participants
+    _addUser(); 
+    //if a key is in participants but not snapshot
+    _removeUser(); 
   }
 
   const _addUser = () => { 
