99d7ecb347f17e2132287c8b3f8e3d7c3fa7a5ef
Sean Nealon <signalflowsean@gmail.com>
2019-03-29 14:03:43 -0400

    added eslint

--- js/utils/_userManagement.js
+++ js/utils/_userManagement.js
@@ -1,70 +1,73 @@
+/*global manageUsers*/
 const manageUsers = () => { 
-  const currUsers = {}; 
+
   const checkForUpdates = () => { 
-    if (dancer.isReady){ 
-      let timeToInvoke = 3000; 
+		if (dancer.isReady){ 
+			let timeToInvoke = 3000 
       //reads users from fire base every x milliseconds
-      
-      setInterval(_checkUserList, timeToInvoke);  
+      setInterval(_checkUserList, timeToInvoke)  
     }   
-  }
+	}
 
-  const _checkUserList = () => { 
-    firebase
-      .database()
-      .ref('users')
-      .once('value')
-      .then(snapshot => {
-        //number of firebase participants
-        const numFBParticipants = (Object.keys(snapshot.val()).length -1); 
+	const _checkUserList = () => { 
+		firebase
+			.database()
+			.ref("users")
+			.once("value")
+			.then(snapshot => {
+				//number of firebase participants
+				const numFBParticipants = (Object.keys(snapshot.val()).length -1) 
         // _printNumParticipants(numFBParticipants); 
-        _areParticipantsLengthEqual(numFBParticipants, participants.length, snapshot.val()); 
-      });  
+        _areParticipantsLengthEqual(numFBParticipants, participants.length, snapshot.val()) 
+      })  
   }
 
-  const _printNumParticipants = (FBParticipants) => { 
-    console.log('num firebase-participants:', FBParticipants, 
-      'num local-participants:', participants.length); 
+	const _printNumParticipants = (FBParticipants) => { 
+		console.log("num firebase-participants:", FBParticipants, 
+			"num local-participants:", participants.length) 
   }
 
-  const _areParticipantsLengthEqual = (fbPrtcpntsLength, locPrtcpntsLength, fbParticipants) => { 
-    if (fbPrtcpntsLength !== locPrtcpntsLength) { 
-      console.log("the number of user's has changed"); 
-      _numUsersHasChanged(fbPrtcpntsLength, locPrtcpntsLength, fbParticipants); 
+	const _areParticipantsLengthEqual = (fbPrtcpntsLength, locPrtcpntsLength, fbParticipants) => { 
+		if (fbPrtcpntsLength !== locPrtcpntsLength) { 
+			console.log("the number of user's has changed") 
+      _numUsersHasChanged(fbPrtcpntsLength, locPrtcpntsLength, fbParticipants) 
     }
-  }
+	}
 
-  const _numUsersHasChanged = (fbPrtcpntsLength, locPrtcpntsLength, fbParticipants) => { 
-    //if a key is in snapshot but not participants
-    const fbParticipantsKeys = Object.keys(fbParticipants); 
+	const _numUsersHasChanged = (fbPrtcpntsLength, locPrtcpntsLength, fbParticipants) => { 
+		//if a key is in snapshot but not participants
+		const fbParticipantsKeys = Object.keys(fbParticipants) 
     if (fbPrtcpntsLength > locPrtcpntsLength){ 
-      for (let i = 0; i < fbPrtcpntsLength; i++){ 
-        console.log('a user has been added'); 
-        console.log('participants', participants); 
-        console.log('fbParticipants', fbParticipantsKeys[i], 'participants', participants,  participants[i]); 
-        if (fbParticipantsKeys[i] !== participants[i]){ 
-          let newPartcipant = Participant(); 
-          newPartcipant.id = fbParticipantsKeys[i]; 
-          newPartcipant.pose = firebase.database().ref(`users/${this.id}`); 
-          console.log('new participant', newPartcipant.pose); 
-          console.log('newParticipant', newPartcipant);
-          participants.push(newPartcipant); 
-          return; 
-        }
-      }
-    }
+			for (let i = 0; i < fbPrtcpntsLength; i++){ 
 
-    //if a key is in participants but not snapshot
-    else if (fbPrtcpntsLength < locPrtcpntsLength){ 
-      for (let i = 0; i < locPrtcpntsLength; i++){ 
-        if (fbParticipantsKeys[i] !== participants[i]){ 
-          console.log('removing'); 
-          participants.splice(i, 1); 
-          return; 
-        }
-      }
-    }
-  }
+				console.log("participants", participants) 
+        console.log("fbParticipants", fbParticipantsKeys[i], "participants", participants,  participants[i]) 
 
-  return { checkForUpdates }
+        if (fbParticipantsKeys[i] !== participants[i]){ 
+					let newPartcipant = Participant() 
+          firebase.database().ref(`users/${this.id}`) 
+						.once("value")
+						.then(snapshot => {
+							newPartcipant.id = fbParticipantsKeys[i] 
+              newParticipant.pose = snapshot.val().pose
+              participants.push(newPartcipant) 
+              return; 
+						})  
+        }
+			}
+		}
+
+		//if a key is in participants but not snapshot
+		else if (fbPrtcpntsLength < locPrtcpntsLength){ 
+			for (let i = 0; i < locPrtcpntsLength; i++){ 
+				if (fbParticipantsKeys[i] !== participants[i]){ 
+					console.log("removing") 
+          participants.splice(i, 1) 
+          return; 
+				}
+			}
+		}
+	}
+
+	return { checkForUpdates }
 }
